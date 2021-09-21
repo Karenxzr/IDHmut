a2052fa1863941c08a9a6b75a6abf0442a351c4f
Zhuoran Xu <cyranvvv@hotmail.com>
2021-09-21 19:02:54 -0400

    Update Tilingsvs.py

--- Preprocess/Tilingsvs.py
+++ Preprocess/Tilingsvs.py
@@ -150,9 +150,55 @@ def tiling_qualified_separate_10x(svspath,targetpath,tilesize,stride, tissuepct_
                 np.save(filename, tile)
     slide.close()
 
+def tiling_qualified_separate_20x(svspath,targetpath,tilesize,stride, tissuepct_value=0.8):
+    """
+    tiling WSI based on fixed threshold, tiling 10x specifically
 
+    svspath: svs path of input image
+    tilesize: tilesize at level 2
+    stride: at level 0, set to tilesize if no overlapping
+    targetpath: location to save patches from each slide. ex. 'patches/TCGA-XX-XXXX/'
+    return: save each patch seperately
+    """
+    # use thumbnail image at level 3, downsizing factor: 64
+    slide = openslide.open_slide(svspath)
+    m=round(float(slide.properties['aperio.MPP']), 2)//0.25
+    level0=int(40/m)
+
+    bestlevel=0
+    for i in range(slide.level_count):
+        if round(level0/ slide.level_downsamples[i], 1) // 10 > 0:
+            bestlevel = i
+    rescale_factor=round(level0 / slide.level_downsamples[bestlevel],1)/10
+
+    input_img = np.array(slide.read_region((0, 0), bestlevel, slide.level_dimensions[bestlevel]))[:, :, :3]
+    if rescale_factor>1:
+        input_img = np.round(rescale(input_img, 1/rescale_factor, multichannel=True, preserve_range=True, anti_aliasing=True)).astype(np.uint8)
+    img_h, img_w = input_img.shape[:2]
+
+        # calculate tile numbers (step_num_h*step_num_w)
+    img_h_adj = img_h - tilesize
+    img_w_adj = img_w - tilesize
+    step_num_h = math.floor(img_h_adj / stride) + 1
+    step_num_w = math.floor(img_w_adj / stride) + 1
+    print('tile number: ' + str(step_num_h * step_num_w))
+
+    tile_coord = []  # record coordinates at level 0 scale
+    for w in range(step_num_w):
+        for h in range(step_num_h):
+            x = int(w * stride)
+            y = int(h * stride)
+            sub_img = input_img[y:y+tilesize,x:x+tilesize,:]
+            # QC
+            if filter.tissue_pct(sub_img) > tissuepct_value:
+                tile_coord.append((x, y))
+                tile = sub_img
+                #np.save(targetpath +'DX_'+manage.DX(svspath)+'_'+str(int(x*16)) + '_' + str(int(y*16)) + '.npy', tile)
+                filename = os.path.join(targetpath, str(int(x * 4)) + '_' + str(int(y * 4)) + '.npy')
+                np.save(filename, tile)
+    slide.close()
     
-def tiling_qualified_separate_20x(svspath,targetpath,tilesize,stride, tissuepct_value=0.8,  h_factor=2,offset=0):
+def tiling_qualified_separate_20x(svspath,targetpath,tilesize,stride,tissuepct_value=0.8,h_factor=2,offset=0):
     """
     tiling WSI based on fixed threshold, tiling 20x specifically
     """
