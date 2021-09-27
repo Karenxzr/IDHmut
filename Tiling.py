9614efa6e8ae3dc02ef14eef7f5d914cbedc4c56
Zhuoran Xu <cyranvvv@hotmail.com>
2021-09-27 17:32:29 -0400

    Update Tiling.py

--- Tiling.py
+++ Tiling.py
@@ -38,25 +38,33 @@ def tiling(svspath,patientid,targetpath,tilesize=256,stride=256,tissuepct_value=
         if not os.path.isdir(patch_path2_5x):
             os.makedirs(patch_path2_5x)
         Tilingsvs.tiling_qualified_separate_2_5x(svspath=svspath,targetpath=patch_path2_5x,tilesize=tilesize,stride=stride, tissuepct_value=tissuepct_value)
+        if len(manage.list_npy(patch_path2_5x))==0:
+            os.rmdir(patch_path2_5x)
     
     if '5x' in mag:
         patch_path5x = os.path.join(targetpath, '5x', patientid)
         if not os.path.isdir(patch_path5x):
             os.makedirs(patch_path5x)
         Tilingsvs.tiling_qualified_separate_5x(svspath=svspath,targetpath=patch_path5x,tilesize=tilesize,stride=stride, tissuepct_value=tissuepct_value)
+        if len(manage.list_npy(patch_path5x))==0:
+            os.rmdir(patch_path5x)
             
     if '10x' in mag:
         patch_path10x = os.path.join(targetpath, '10x', patientid)
         if not os.path.isdir(patch_path10x):
             os.makedirs(patch_path10x)
         Tilingsvs.tiling_qualified_separate_10x(svspath=svspath,targetpath=patch_path10x,tilesize=tilesize,stride=stride, tissuepct_value=tissuepct_value)
+        if len(manage.list_npy(patch_path10x))==0:
+            os.rmdir(patch_path10x)
+            
             
     if '20x' in mag:
         patch_path20x = os.path.join(targetpath, '20x', patientid)
         if not os.path.isdir(patch_path20x):
             os.makedirs(patch_path20x)
         Tilingsvs.tiling_qualified_separate_20x(svspath=svspath,targetpath=patch_path20x,tilesize=tilesize,stride=stride, tissuepct_value=tissuepct_value)
-            
+        if len(manage.list_npy(patch_path20x))==0:
+            os.rmdir(patch_path20x)   
 
 
 def main():
